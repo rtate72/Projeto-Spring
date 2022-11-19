1488286a3591a0f0e9175a64557cb8337044c54d
Rtate72 <rtate@terra.com.br>
2022-11-19 00:01:43 -0300

    First Commit

---
+++ src/main/java/com/dio/projetospring/service/ClienteService.java
@@ -0,0 +1,18 @@
+package com.dio.projetospring.service;
+
+import com.dio.projetospring.model.Cliente;
+
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
---
+++ src/main/java/com/dio/projetospring/service/ViaCepService.java
@@ -0,0 +1,15 @@
+package com.dio.projetospring.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.dio.projetospring.model.Endereco;
+
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
